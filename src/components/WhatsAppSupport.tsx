59b4ff9924a3acbc10b5c2cc483e2d8b45bb35e2
Vico <kathybuck3350@gmail.com>
2025-12-03 11:52:39 +0000

    update ***

--- src/components/WhatsAppSupport.tsx
+++ src/components/WhatsAppSupport.tsx
@@ -8,7 +8,7 @@ export const WhatsAppSupport = () => {
     const message = 'Hi, I need help reporting a scam.'
     const handleClick = () => {
         const encodedMessage = encodeURIComponent(message);
-        window.open(`https://wa.link/2s3pvy`, '_blank');
+        window.open(`https://wa.me/14693266824`, '_blank');
     };
 
     return (
