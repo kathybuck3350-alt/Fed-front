14dc969189550875d7f9ae5916f7a20d7065af6b
Vico <kathybuck3350@gmail.com>
2026-02-26 18:11:00 +0000

    build ***

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // <--- Important! Ensures proper paths for Netlify
   server: {
     host: "::",
     port: 8080,
